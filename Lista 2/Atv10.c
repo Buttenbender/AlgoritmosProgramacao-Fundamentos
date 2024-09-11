69345fd80327788590bc51d81b5eac96b3343c2a
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-11 20:04:34 -0300

    Update Atv10.c

--- Lista 2/Atv10.c
+++ Lista 2/Atv10.c
@@ -1,6 +1,10 @@
 /*
 * Programa: Atv10.c
-* Descrição: calcula quanto o cliente gastou e aplica o desconto
+* Descrição: O lojista gostou tanto do seu programa anterior que encomendou outro. Dessa vez ele quer
+* que você calcule quanto cada cliente gastou na loja apenas informando o número de
+* camisetas, calças e cintos comprados. As camisetas custam R$ 25,00, as calças cem reais e os
+* cintos 40 reais. Some o valor da compra e ao final dê um desconto de 10 por cento sobre o
+* total. Exiba na tela o valor do desconto e o valor da compra.
 * Autor: João Pedro Büttenbender da Silva
 * Data de criação: 02/09/2024
 * Última modificação: 02/09/2024
