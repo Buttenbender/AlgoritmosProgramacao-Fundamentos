2796150e9756b16cbde8c0ee3de3b3297610b9b9
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-11 20:05:39 -0300

    Update Atv3.c

--- Lista 2/Atv3.c
+++ Lista 2/Atv3.c
@@ -1,6 +1,7 @@
 /*
 * Programa: Atv3.c
-* Descrição: Calcula o preço de um prato pelo peso
+* Descrição: Um restaurante de buffet a quilo cobra R$ 40,00 por quilo. Escreva um programa que leia o
+* peso do prato do cliente e calcule o valor a ser pago.
 * Autor: João Pedro Büttenbender da Silva
 * Data de criação: 02/09/2024
 * Última modificação: 02/09/2024
