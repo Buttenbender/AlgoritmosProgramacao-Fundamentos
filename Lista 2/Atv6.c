cd4322457bda1bd0aee8dc430fecea2e49c6c17c
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-11 20:07:25 -0300

    Update Atv6.c

--- Lista 2/Atv6.c
+++ Lista 2/Atv6.c
@@ -1,6 +1,10 @@
 /*
 * Programa: Atv6.c
-* Descrição: calcula quantos reais em tablets e telefones foram vendidos
+* Descrição: A loja de eletrônicos TechMundo vende uma certa quantidade de smartphones e uma
+* quantidade de tablets a cada dia. Cada smartphone custa R$ 1000,00 e cada tablet custa R$
+* 1500,00. Ao final do dia, o dono quer saber quanto arrecadou com a venda dos smartphones
+* e dos tablets. Escreva um programa que leia o número de smartphones e tablets vendidos em
+* um dia e calcule o total arrecadado.
 * Autor: João Pedro Büttenbender da Silva
 * Data de criação: 02/09/2024
 * Última modificação: 02/09/2024
