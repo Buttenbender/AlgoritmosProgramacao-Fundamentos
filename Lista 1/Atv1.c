0b07a27133fe3c42f74225c491f14776c9dd9fbf
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-11 20:00:00 -0300

    Update Atv1.c

--- Lista 1/Atv1.c
+++ Lista 1/Atv1.c
@@ -1,6 +1,8 @@
 /*
 * Programa: Atv1.c
-* Descrição: Programa que imprime "Olá Mundo!" em C
+* Descrição: Utilizando o OnlineGDB, pesquise e implemente um programa que escreva na tela “Olá
+* Mundo!” em 3 linguagens de programação diferentes. Qual é o comando de saída de dados
+* nestas 3 linguagens?
 * Autor: João Pedro Bütenbender da Silva
 * Data de criação: 29/08/2024
 * Última modificação: 29/08/2024
