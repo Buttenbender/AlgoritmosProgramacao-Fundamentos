b945e0d216f590c08b6db25f5f89019cdcc29940
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-11 20:00:26 -0300

    Update Atv1.java

--- Lista 1/Atv1.java
+++ Lista 1/Atv1.java
@@ -1,6 +1,8 @@
 /*
 * Programa: Atv1.java
-* Descrição: Programa que imprime "Olá Mundo!" em java
+* Descrição: Utilizando o OnlineGDB, pesquise e implemente um programa que escreva na tela “Olá
+* Mundo!” em 3 linguagens de programação diferentes. Qual é o comando de saída de dados
+* nestas 3 linguagens?
 * Autor: João Pedro Büttenbender da Silva
 * Data de criação: 29/08/2024
 * Última modificação: 29/08/2024
