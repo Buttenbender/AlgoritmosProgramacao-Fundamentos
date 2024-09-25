728dbcbead507388be04734712c36a318dbb5d77
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-25 19:32:40 -0300

    Update Atv6.c

--- Lista 3/Atv6.c
+++ Lista 3/Atv6.c
@@ -1,3 +1,15 @@
+/*
+* Programa: Atv6.c
+* Descrição: Brincadeira do PAR ou ÍMPAR. Pergunte para o usuário se ele aposta em PAR ou ÍMPAR. Depois
+* disso, peça para ele digitar um número de 0 a 5 (como se fosse mostrar os dedos da mão. Sorteie
+* um número de 0 a 5 e some com o que o usuário digitou. Se o usuário escolheu PAR e o valor da
+* soma for par OU se ele escolheu ÍMPAR e o valor da soma é ímpar, diga que ele venceu. Senão, diga
+* que o programa venceu
+* Autor: João Pedro Büttenbender da Silva
+* Data de criação: 24/09/2024
+* Última modificação: 24/09/2024
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
