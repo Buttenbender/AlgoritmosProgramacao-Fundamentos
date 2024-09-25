118a0287719860c63663fe5b6c0becd922b0c5b5
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-25 19:28:23 -0300

    Update Atv14.c

--- Lista 3/Atv14.c
+++ Lista 3/Atv14.c
@@ -1,3 +1,17 @@
+/*
+* Programa: Atv14.c
+* Descrição: Criar um programa para identificar o valor a ser pago por um plano de saúde dada a idade do
+* conveniado considerando que todos pagam R$ 300 e mais um adicional (se tiver dependentes)
+* conforme a seguinte tabela:
+* a) crianças com menos de 10 anos pagam R$100;
+* b) dependentes com idade entre 10 e 30 anos pagam R$220;
+* c) dependentes com idade entre 31 e 60 anos pagam R$ 395; e
+* d) dependentes com mais de 60 anos pagam R$ 480.
+* Autor: João Pedro Büttenbender da Silva
+* Data de criação: 24/09/2024
+* Última modificação: 24/09/2024
+*/
+
 #include <stdio.h>
 
 int main() {
