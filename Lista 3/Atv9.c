48b5932f8003cc40bf3bf9c89a1836abff51d352
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-25 19:35:20 -0300

    Update Atv9.c

--- Lista 3/Atv9.c
+++ Lista 3/Atv9.c
@@ -1,3 +1,20 @@
+/*
+* Programa: Atv9.c
+* Descrição: Faça um conversor de câmbio de reais/dólar/euro. O usuário deve informar inicialmente a cotação
+* de cada moeda em relação ao real. Depois apresente o seguinte menu:
+*1) Converter de Real para Euro
+* 2) Converter de Real para Dólar
+* 3) Converter de Euro para Dólar
+* 4) Converter de Euro para Real
+* 5) Converter de Dólar para Euro
+* 6) Converter de Dólar para Real
+* Leia o valor a ser convertido na moeda de origem e imprima na tela a quantidade na moeda
+* destino
+* Autor: João Pedro Büttenbender da Silva
+* Data de criação: 24/09/2024
+* Última modificação: 24/09/2024
+*/
+
 #include <stdio.h>
 
 int main() {
