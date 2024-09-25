1ab7f2b45d0489d2ee0d142d5000fe6829262adf
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-25 19:27:08 -0300

    Update Atv13.c

--- Lista 3/Atv13.c
+++ Lista 3/Atv13.c
@@ -1,3 +1,15 @@
+/*
+* Programa: Atv13.c
+* Descrição: Faça um programa que leia a nota do Grau A e do Grau B do aluno e calcule a média final conforme
+* as regras da Unisinos. Imprima a média final na tela e diga se o aluno passou por média ou se ficou
+* em recuperação (grau C). Se o aluno ficou em recuperação, pergunte se ele quer substituir o Grau
+* A ou o Grau B (ele deve digitar ‘a’ ou ‘b’). Leia a nota do Grau C, recalcule a média de acordo com o
+* grau substituído e imprima na tela o resultado, informando se ele foi aprovado ou reprovado
+* Autor: João Pedro Büttenbender da Silva
+* Data de criação: 24/09/2024
+* Última modificação: 24/09/2024
+*/
+
 #include <stdio.h>
 
 int main() {
