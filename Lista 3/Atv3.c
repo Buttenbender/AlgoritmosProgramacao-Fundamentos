c68217d15338c34447c61017dede0d17b90d489c
Buttenbender <168119953+Buttenbender@users.noreply.github.com>
2024-09-25 19:30:53 -0300

    Update Atv3.c

--- Lista 3/Atv3.c
+++ Lista 3/Atv3.c
@@ -1,3 +1,12 @@
+/*
+* Programa: Atv3.c
+* Descrição: Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo
+* o resultado
+* Autor: João Pedro Büttenbender da Silva
+* Data de criação: 24/09/2024
+* Última modificação: 24/09/2024
+*/
+
 #include <stdio.h>
 
 int main() {
